1380c64064a1d807a925cbc1f623f1c40c7bb4cc
YuriBondar <bondar.yur@gmail.com>
2024-07-02 11:11:52 +0200

    first save

--- VisitorsEmployeeDatabase.py
+++ VisitorsEmployeeDatabase.py
@@ -1,1 +1,65 @@
-print("Hello, world!")
+
+if __name__ == "__main__":
+
+    employeesPositions = (
+    "Status: Mitarbeiter*inn oder Besucher*inn",
+    "Nachname",
+    "Vorname",
+    "Telefonnummer",
+    "E-Mail",
+    "Sozialversicherungsnummer",
+    "Arbeitet im Unternehmen seit:",
+    "Position")
+
+    visitorsPositions = (
+        "Status: Mitarbeiter*inn oder Besucher*inn",
+        "Nachname",
+        "Vorname",
+        "Telefonnummer",
+        "E-Mail",
+        "Tag und Zeit des Besuchs"
+        "Verantwortlicher Manager")
+
+    employee = ["", "", "", 0, "", "", "", ""]
+    visitor = ["", "", "", 0, "", "", ""]
+    employeesList = []
+    visitorsList = []
+
+    print("Welcome to database!")
+    while True
+        print("Type 'v' to create new record for visitor")
+        print("Type 'e' to create new record for employee")
+        status = input("Type 'e' to create new record for employee: ")
+        if status == "e":
+            while True:
+                fCreateNewEmployee()
+                if fCheckNewEmployee():
+                    fAddEmployee()
+                    break
+        if status == "v":
+            while True:
+                fCreateNewVisitor()
+                if fCheckVisitor():
+                    break
+            if isEmployeeIndatabase():
+                fAddVisitor()
+                break
+            else:
+                fcorrectVisitor()
+                break
+        else:
+            pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
