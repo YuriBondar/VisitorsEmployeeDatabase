6793eb36cf1d2dc06a447a3847b418667cbfa38a
YuriBondar <bondar.yur@gmail.com>
2024-07-16 15:39:35 +0200

    Auftrag 6 Draft8 with GUI4

--- mainmenuGUI.py
+++ mainmenuGUI.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 
+from EntryMenuGUI import *
 from checkFunctions import *
 from dataFunctions import *
 from tkinter import messagebox
@@ -17,67 +18,7 @@ def backToMainMenu(window):
 #-----------------------------------------------------------------------------------
 #-----------------------------------------------------------------------------------
 #------------------------entry menu-------------------------------------------------
-def delEntries(entries):
-    for entry in entries:
-        entry.delete(0,tk.END)
 
-def entryPerson(entries, typeOfPerson):
-    atributesList = chooseAtributesList(typeOfPerson)
-    newPerson = []
-    for entry in entries:
-        newPerson.append(entry.get())
-    for i in range(len(newPerson)-1):
-        if checkData(i+1, newPerson[i], typeOfPerson) is not True:
-            errorMessage = checkData(i+1, newPerson[i], typeOfPerson)
-            currentPerson = atributesList[i]
-            messagebox.showerror("Error", f" Field {currentPerson} : {errorMessage}")
-            return
-
-    addRecordtoFile(newPerson, typeOfPerson)
-    messagebox.showinfo("Dates are saved succesful",)
-    delEntries(entries)
-
-def addNewRecordGUI(windowMain, typeOfPerson):
-    atributesList = chooseAtributesList(typeOfPerson)
-    windowAdd = tk.Tk()
-    windowMain.destroy()
-    windowAdd.grid_columnconfigure(0, weight=1)
-    windowAdd.grid_rowconfigure(0, weight=1)
-    windowAdd.grid_rowconfigure(1, weight=1)
-#---------------------------------------------------------------------------------------------------------
-    firstLabelInFrame(windowAdd, f"Geben Sie die Daten des {translateTypeOfPerson(typeOfPerson)} an:")
-#----------------------------------------------------------------------------------------------------------
-    frame2 = tk.Frame(windowAdd, relief="raised", borderwidth=4)
-    frame2.grid(row=1, column=0, padx=10, pady=10, sticky="ew")
-
-    entries = []
-
-    for i in range(len(atributesList)):
-        label = tk.Label(frame2, text=atributesList[i], bg="light gray")
-        label.grid(row=i, column=0, padx=15, pady=10, sticky="ew")
-        entry = tk.Entry(frame2)
-        entry.grid(row=i, column=1, padx=15, pady=10, sticky="ew")
-        entries.append(entry)
-
-# ----------------------------------------------------------------------------------------------------------
-    frame3 = tk.Frame(windowAdd, relief="raised", borderwidth=4)
-    frame3.grid(row=2, column=0, padx=10, pady=10, sticky="ew")
-    frame3.grid_columnconfigure(0, weight=1)
-    frame3.grid_rowconfigure(0, weight=1)
-    frame3.grid_rowconfigure(1, weight=1)
-    frame3.grid_columnconfigure(1, weight=1)
-
-
-    buttonSave = tk.Button(frame3, text="Daten Speichern", command=lambda: entryPerson(entries,typeOfPerson))
-    buttonSave.grid(row=0, column=0, padx=15, pady=10, sticky="ew")
-
-    buttonMainMenu = tk.Button(frame3, text="Back to Main Menu", command=lambda: backToMainMenu(windowAdd))
-    buttonMainMenu.grid(row=0, column=1, padx=15, pady=10, sticky="ew")
-
-    buttonDelete = tk.Button(frame3, text="delete Dates", command=lambda: delEntries(entries))
-    buttonDelete.grid(row=1, column=1, padx=15, pady=10, sticky="ew")
-
-    windowAdd.mainloop()
 
 def selectRecordsGUI(windowMain, typeOfPerson):
     pass
@@ -89,9 +30,9 @@ def printFullDatabaseGUI(windowMain,typeOfPerson):
     atributesList = chooseAtributesList(typeOfPerson)
     windowPrint = tk.Tk()
     windowMain.destroy()
-    windowPrint .grid_columnconfigure(0, weight=1)
-    windowPrint .grid_rowconfigure(0, weight=1)
-    windowPrint .grid_rowconfigure(1, weight=1)
+    windowPrint.grid_columnconfigure(0, weight=1)
+    windowPrint.grid_rowconfigure(0, weight=1)
+    windowPrint.grid_rowconfigure(1, weight=1)
     pass
 
 
